c96b65a30023bfc512861823cdfec682fbc6c4d5
Batool sharafaldeen <101997272+batoly@users.noreply.github.com>
2022-03-24 22:05:54 +0300

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Lenovo on 3/19/2022.
+ */
+public class Q5 {
+    static class Node{
+        char data;
+        Node prev;
+        Node next;
+    }
+    static Node head=null;
+    static void rotate(int S){
+        if (S==0)
+            return;
+    }
+}
